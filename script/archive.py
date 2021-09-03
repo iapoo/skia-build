f1af4749e13eeb50d0868c578c2d5eb2991d0022
knziha <302772670@qq.com>
2021-09-03 14:49:24 +0800

    fuck python

--- script/archive.py
+++ script/archive.py
@@ -1,43 +1,51 @@
 #! /usr/bin/env python3
 
 import common, os, pathlib, sys, zipfile
+from os.path import *
+
 
 def parents(path):
-  res = []
-  parent = path.parent
-  while '.' != str(parent):
-    res.insert(0, parent)
-    parent = parent.parent
-  return res
+    res = []
+    parent = path.parent
+    while '.' != str(parent):
+        res.insert(0, parent)
+        parent = parent.parent
+    return res
+
 
 def main():
-  os.chdir(os.path.join(os.path.dirname(__file__), os.pardir, 'skia'))
-  
-  build_type = common.build_type()
-  version = common.version()
-  machine = common.machine()
-  system = common.system()
-  classifier = common.classifier()
+    os.chdir(os.path.join(os.path.dirname(__file__), os.pardir, 'skia'))
 
-  globs = [
-    '[!.git]*/*'
-  ]
+    build_type = common.build_type()
+    version = common.version()
+    machine = common.machine()
+    system = common.system()
+    classifier = common.classifier()
 
-  target = 'Skia-' + version + '-' + system + '-' + build_type + '-' + machine + classifier + '.zip'
-  print('> Writing', target)
-  
-  with zipfile.ZipFile(os.path.join(os.pardir, target), 'w', compression=zipfile.ZIP_DEFLATED) as zip:
-    dirs = set()
-    for glob in globs:
-      for path in pathlib.Path().glob(glob):
-        if not path.is_dir():
-          for dir in parents(path):
-            if not dir in dirs :
-              zip.write(str(dir))
-              dirs.add(dir)
-          zip.write(str(path))
+    globs = [
+        '[!.git]*/*'
+    ]
 
-  return 0
+    target = 'Skia-' + version + '-' + system + '-' + build_type + '-' + machine + classifier + '.zip'
+    print('> Writing', target)
+
+    rootdir = split(realpath(__file__))[0]
+    list_ = [rootdir]
+    zip = zipfile.ZipFile(os.path.join(os.pardir, target), 'w', compression=zipfile.ZIP_DEFLATED)
+    while len(list_) > 0:
+        dir_ = list_.pop()
+        files_ = os.listdir(dir_)
+        for i in range(0, len(files_)):
+            path = os.path.join(dir_, files_[i])
+            if os.path.isdir(path):
+                if not files_[i] == ".git":
+                    list_.append(path)
+            else:
+                #print(path[len(rootdir) + 1:])
+                zip.write(path, path[len(rootdir) + 1:])
+
+    return 0
+
 
 if __name__ == '__main__':
-  sys.exit(main())
+    sys.exit(main())
