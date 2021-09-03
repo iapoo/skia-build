4af6abf1ea68eb236588777a6bc9a5d06a4f4919
knziha <302772670@qq.com>
2021-09-03 21:10:57 +0800

    Update archive.py

--- script/archive.py
+++ script/archive.py
@@ -25,26 +25,82 @@ def main():
     classifier = common.classifier()
 
     globs = [
-        '[!.git]*/*'
+        'out/' + build_type + '-' + machine + '/*.a',
+        'out/' + build_type + '-' + machine + '/*.lib',
+        'out/' + build_type + '-' + machine + '/icudtl.dat',
+        'include/**/*',
+        'modules/particles/include/*.h',
+        'modules/skottie/include/*.h',
+        'modules/skottie/src/*.h',
+        'modules/skottie/src/animator/*.h',
+        'modules/skottie/src/effects/*.h',
+        'modules/skottie/src/layers/*.h',
+        'modules/skottie/src/layers/shapelayer/*.h',
+        'modules/skottie/src/text/*.h',
+        'modules/skparagraph/include/*.h',
+        'modules/skplaintexteditor/include/*.h',
+        'modules/skresources/include/*.h',
+        'modules/sksg/include/*.h',
+        'modules/skshaper/include/*.h',
+        'modules/skshaper/src/*.h',
+        'modules/svg/include/*.h',
+        'src/core/*.h',
+        'src/gpu/gl/*.h',
+        'src/utils/*.h',
+        'third_party/externals/angle2/LICENSE',
+        'third_party/externals/angle2/include/**/*',
+        'third_party/externals/freetype/docs/FTL.TXT',
+        'third_party/externals/freetype/docs/GPLv2.TXT',
+        'third_party/externals/freetype/docs/LICENSE.TXT',
+        'third_party/externals/freetype/include/**/*',
+        'third_party/externals/icu/source/common/**/*.h',
+        'third_party/externals/libpng/LICENSE',
+        'third_party/externals/libpng/*.h',
+        'third_party/externals/libwebp/COPYING',
+        'third_party/externals/libwebp/PATENTS',
+        'third_party/externals/libwebp/src/dec/*.h',
+        'third_party/externals/libwebp/src/dsp/*.h',
+        'third_party/externals/libwebp/src/enc/*.h',
+        'third_party/externals/libwebp/src/mux/*.h',
+        'third_party/externals/libwebp/src/utils/*.h',
+        'third_party/externals/libwebp/src/webp/*.h',
+        'third_party/externals/harfbuzz/COPYING',
+        'third_party/externals/harfbuzz/src/*.h',
+        'third_party/externals/swiftshader/LICENSE.txt',
+        'third_party/externals/swiftshader/include/**/*',
+        'third_party/externals/zlib/LICENSE',
+        'third_party/externals/zlib/*.h',
+        "third_party/icu/*.h"
     ]
 
     target = 'Skia-' + version + '-' + system + '-' + build_type + '-' + machine + classifier + '.zip'
     print('> Writing', target)
 
     rootdir = os.path.abspath(os.getcwd())
-    list_ = [rootdir]
     zip = zipfile.ZipFile(os.path.join(os.pardir, target), 'w', compression=zipfile.ZIP_DEFLATED)
-    while len(list_) > 0:
-        dir_ = list_.pop()
-        files_ = os.listdir(dir_)
-        for i in range(0, len(files_)):
-            path = os.path.join(dir_, files_[i])
-            if os.path.isdir(path):
-                if not files_[i] == ".git":
-                    list_.append(path)
-            else:
-                #print(path[len(rootdir) + 1:])
-                zip.write(path, path[len(rootdir) + 1:])
+    if True:
+        dirs = set()
+        for glob in globs:
+            for path in pathlib.Path().glob(glob):
+                if not path.is_dir():
+                    for dir in parents(path):
+                        if not dir in dirs:
+                            zip.write(str(dir))
+                            dirs.add(dir)
+                    zip.write(str(path))
+    else:
+        list_ = [rootdir]
+        while len(list_) > 0:
+            dir_ = list_.pop()
+            files_ = os.listdir(dir_)
+            for i in range(0, len(files_)):
+                path = os.path.join(dir_, files_[i])
+                if os.path.isdir(path):
+                    if not files_[i] == ".git":
+                        list_.append(path)
+                else:
+                    # print(path[len(rootdir) + 1:])
+                    zip.write(path, path[len(rootdir) + 1:])
 
     return 0
 
